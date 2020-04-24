2147f17abc92ff6e92d5daa3cf36b41e291889df
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2020-04-24 21:09:26 +0300

    Update config.py

--- myapp/config.py
+++ myapp/config.py
@@ -4,12 +4,12 @@ import os
 class Config:
     
     SECRET_KEY='842551071e8a5f7a16f9029e37863f4a'#os.environ.get('SECRET_KEY')#
-    SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'  #'postgresql://sanix:19972017Russia@localhost/db'
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'  
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'TechyIntelo'#os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = '19972017Russia'#os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SIMPLEMDE_jS_IIFE=True
     SIMPLEMDE_USE_CDN=True
     EKO_MAIL_SUBJECT_PREFIX='[EKOB]'
